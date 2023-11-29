c8dc511dbf48e8a21c508458efae3895f08b8a9d
GUAN-MING <guanmingchiu@gmail.com>
2023-11-29 21:47:42 +0800

    chore: remove unused functions

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,23 +1,10 @@
-import { useMemo } from "react";
-
 import { useSession } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
-
-import { getAvatar } from "@/lib/utils";
 
 // this is a helper function to get user info in client components
 export default function useUserInfo() {
-  const searchParams = useSearchParams();
-  const username = useMemo(() => searchParams.get("username"), [searchParams]);
-  const handle = useMemo(() => searchParams.get("handle"), [searchParams]);
-  const avatarURL = useMemo(() => getAvatar(username), [username]);
-
   const { data: session } = useSession();
 
   return {
-    username,
-    handle,
-    avatarURL,
     session,
   };
 }
