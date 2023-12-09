7b5867473189ebd0d67dfd90d5e520e302ed7d33
GUAN-MING <guanmingchiu@gmail.com>
2023-12-09 17:09:08 +0800

    refactor: separate announcementList from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 
-import { eq, desc } from "drizzle-orm";
-import { User } from "lucide-react";
-
 import Header from "@/components/Header";
-import TimeText from "@/components/Timetext";
-import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import { Separator } from "@/components/ui/separator";
 import { Toaster } from "@/components/ui/toaster";
 import NextAuthProvider from "@/context/NextAuthProvider";
-import { db } from "@/db";
-import { announcementTable, userTable } from "@/db/schema";
 
 import "./globals.css";
+import AnnouncementList from "@/components/AnnouncementList";
 
 const noto = Noto_Sans({
   subsets: ["latin-ext"],
@@ -32,19 +26,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const announcementData = await db
-    .select({
-      id: announcementTable.id,
-      content: announcementTable.content,
-      createdAt: announcementTable.createdAt,
-      username: userTable.name,
-    })
-    .from(announcementTable)
-    .innerJoin(userTable, eq(announcementTable.userId, userTable.id))
-    .orderBy(desc(announcementTable.createdAt))
-    .limit(5)
-    .execute();
-
   return (
     <html lang="en">
       <NextAuthProvider>
@@ -56,33 +37,7 @@ export default async function RootLayout({
               <div className="mt-5 w-full">{children}</div>
 
               <Separator orientation="vertical" />
-              <div className="mt-3">
-                <h1 className="flex items-center space-x-2 bg-white px-4 py-4 pt-5 text-lg font-semibold">
-                  <p>Announcement</p>
-                </h1>
-                <div className="px-4">
-                  {announcementData.map((announcement) => (
-                    <Alert
-                      key={announcement.id}
-                      className="mb-3 min-w-[200px] align-middle drop-shadow-sm"
-                    >
-                      <User className="ml-1 h-5 w-5" />
-                      <AlertTitle className="mb-3 ml-2">
-                        {announcement.content}
-                      </AlertTitle>
-                      <AlertDescription className="ml-2 text-xs text-gray-500">
-                        @ {announcement.username}
-                        <p>
-                          <TimeText
-                            date={announcement.createdAt}
-                            format="MM/DD h:mm A"
-                          />
-                        </p>
-                      </AlertDescription>
-                    </Alert>
-                  ))}
-                </div>
-              </div>
+              <AnnouncementList />
             </main>
           </div>
           <Toaster />
---
+++ src/components/AnnouncementList.tsx
@@ -0,0 +1,51 @@
+import { eq, desc } from "drizzle-orm";
+import { User } from "lucide-react";
+
+import TimeText from "@/components/Timetext";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+
+import { db } from "@/db";
+import { announcementTable, userTable } from "@/db/schema";
+
+
+export default async function AnnouncementList() {
+  const announcementData = await db
+    .select({
+      id: announcementTable.id,
+      content: announcementTable.content,
+      createdAt: announcementTable.createdAt,
+      username: userTable.name,
+    })
+    .from(announcementTable)
+    .innerJoin(userTable, eq(announcementTable.userId, userTable.id))
+    .orderBy(desc(announcementTable.createdAt))
+    .limit(5)
+    .execute();
+
+  return (
+    <div className="mt-3">
+      <h1 className="flex items-center space-x-2 bg-white px-4 py-4 pt-5 text-lg font-semibold">
+        <p>🔔 &nbsp;Announcement</p>
+      </h1>
+      <div className="px-4">
+        {announcementData.map((announcement) => (
+          <Alert
+            key={announcement.id}
+            className="mb-3 min-w-[200px] align-middle drop-shadow-sm"
+          >
+            <User className="ml-1 h-5 w-5" />
+            <AlertTitle className="mb-3 ml-2">
+              {announcement.content}
+            </AlertTitle>
+            <AlertDescription className="ml-2 text-xs text-gray-500">
+              @ {announcement.username}
+              <p>
+                <TimeText date={announcement.createdAt} format="MM/DD h:mm A" />
+              </p>
+            </AlertDescription>
+          </Alert>
+        ))}
+      </div>
+    </div>
+  );
+}
